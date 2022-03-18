d438b34b7643b9e5d64a66934df03b233ea34d25
Codel1417 <13484789+Codel1417@users.noreply.github.com>
2022-03-17 22:05:44 -0700

    Remove unnecessary checks and reduce memory allocation

--- VRC-OSC-AudioEars/Program.cs
+++ VRC-OSC-AudioEars/Program.cs
@@ -1,62 +1,65 @@
-﻿
-using BuildSoft.VRChat.Osc;
+﻿using BuildSoft.VRChat.Osc;
 using BuildSoft.VRChat.Osc.Avatar;
 using NAudio.CoreAudioApi;
+using NAudio.Wave;
 using VRC_OSC_AudioEars;
 
 
 const string audioVolumeParameter = "audio_volume";
 const string audioDirectionParameter = "audio_direction";
 
+Console.WriteLine("Starting...");
+
 // Get Default audio device
 MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
 MMDevice device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
-Console.Write("Device: " + device.
-    FriendlyName);
+DirectSoundOut outputDevice = new DirectSoundOut(DirectSoundOut.DSDEVID_DefaultPlayback); // Fixes virtual audio devices by existing?
+
+
+Console.WriteLine("Device: " + device.FriendlyName);
 
 float leftEarSmoothedVolume = 0;
 float rightEarSmoothedVolume = 0;
 float masterSmoothedVolume = 0;
+float leftEarIncomingVolume = 0;
+float rightEarIncomingVolume = 0;
+float masterIncomingVolume = 0;
+float direction = 0;
+
+// ReSharper disable once NotAccessedVariable
+OscAvatarConfig config = await OscAvatarConfig.WaitAndCreateAtCurrentAsync();
+Console.WriteLine("Started");
+
 
 while (true)
 {
-    OscAvatarConfig config = await OscAvatarConfig.WaitAndCreateAtCurrentAsync();
-    if (config.Parameters.ContainsKey(audioDirectionParameter) |
-        config.Parameters.ContainsKey(audioVolumeParameter))
+    try
     {
+        config = await OscAvatarConfig.WaitAndCreateAtCurrentAsync();
 
-        if (device.State == DeviceState.Active && device.AudioMeterInformation.PeakValues.Count > 1)
-        {
-            float leftEarIncomingVolume = device.AudioMeterInformation.PeakValues[0];
-            float rightEarIncomingVolume = device.AudioMeterInformation.PeakValues[1];
-            float masterIncomingVolume = device.AudioMeterInformation.MasterPeakValue;
+        leftEarIncomingVolume = device.AudioMeterInformation.PeakValues[0];
+        rightEarIncomingVolume = device.AudioMeterInformation.PeakValues[1];
+        masterIncomingVolume = device.AudioMeterInformation.MasterPeakValue;
+
+        leftEarSmoothedVolume = Helpers.Lerp(leftEarSmoothedVolume, leftEarIncomingVolume, 0.3f);
+        rightEarSmoothedVolume = Helpers.Lerp(rightEarSmoothedVolume, rightEarIncomingVolume, 0.3f);
+        masterSmoothedVolume = Helpers.Lerp(masterSmoothedVolume, masterIncomingVolume, 0.3f);
+
+        direction = Math.Clamp(-leftEarSmoothedVolume + rightEarSmoothedVolume + 0.5f, 0, 1);
+        //Write all values to the console
+        Console.WriteLine("Left Ear: " + leftEarSmoothedVolume + " Right Ear: " + rightEarSmoothedVolume + " Master: " + masterSmoothedVolume + " Direction: " + direction);
         
-            leftEarSmoothedVolume = Helpers.Lerp(leftEarSmoothedVolume, leftEarIncomingVolume, 0.3f);
-            rightEarSmoothedVolume = Helpers.Lerp(rightEarSmoothedVolume, rightEarIncomingVolume, 0.3f);
-            masterSmoothedVolume = Helpers.Lerp(masterSmoothedVolume, masterIncomingVolume, 0.3f);
-            
-            if (config.Parameters.ContainsKey(audioDirectionParameter))
-            {
-                float direction = Math.Clamp(-leftEarSmoothedVolume + rightEarSmoothedVolume + 0.5f , 0, 1);
-                OscParameter.SendValue("/avatar/parameters/" + audioDirectionParameter, direction);
-            }
-            if (config.Parameters.ContainsKey(audioVolumeParameter))
-            {
-                OscParameter.SendValue("/avatar/parameters/" + audioVolumeParameter, masterSmoothedVolume);
-            }
-        
-        }
-        else
-        {
-            device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
-            Console.Write("Device: " + device.FriendlyName);
-            await Task.Delay(1000);
-        }
+        OscParameter.SendValue("/avatar/parameters/" + audioDirectionParameter, direction);
+
+        OscParameter.SendValue("/avatar/parameters/" + audioVolumeParameter, masterSmoothedVolume);
     }
-    else
+    catch (Exception e)
     {
-        await Task.Delay(1000);
+        Console.WriteLine(e.Message);
+        await Task.Delay(2000);
+
     }
+
+
     await Task.Delay(10);
 }
-
