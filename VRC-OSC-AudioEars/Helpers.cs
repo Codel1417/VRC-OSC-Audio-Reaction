7e243491e426409a30fd949eb5b41be44f2e91a6
Codel1417 <austinrichards98@gmail.com>
2022-04-03 19:52:12 -0700

    Add Logging library, Sentry, and make more methods async

--- VRC-OSC-AudioEars/Helpers.cs
+++ VRC-OSC-AudioEars/Helpers.cs
@@ -1,9 +1,12 @@
-﻿using NAudio.Wave;
-
+﻿using System;
+using System.Reflection;
+using Sentry;
 namespace VRC_OSC_AudioEars
 {
     public static class Helpers
     {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
         /// <summary>
         /// Linear interpolation between two values.
         /// </summary>
@@ -15,7 +18,7 @@ namespace VRC_OSC_AudioEars
         {
             return firstFloat * (1 - by) + secondFloat * by;
         }
-    
+
         /// <summary>
         /// Clamps a float between 0 and 1, while preventing getting too precise when approaching zero. This is due to a bug in VRChat.
         /// </summary>
@@ -30,10 +33,58 @@ namespace VRC_OSC_AudioEars
                 _ => value
             };
         }
+
         public static float VRCClampedLerp(float firstFloat, float secondFloat, float by)
         {
             return VRCClamp(Lerp(firstFloat, secondFloat, by));
         }
-    
+
+        public static string AssemblyProductVersion
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly()
+                    .GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute), false);
+                return attributes.Length == 0
+                    ? ""
+                    : ((AssemblyInformationalVersionAttribute)attributes[0]).InformationalVersion;
+            }
+        }
+
+        public static bool IsDebugBuild
+        {
+            get
+            {
+#if DEBUG
+                    return true;
+#else
+                return false;
+#endif
+            }
+        }
+
+        public static void InitSentry()
+        {
+            SentrySdk.Init(o  =>
+                   {
+                       o.Dsn = "https://c39539385af440b2854d2d558c4d8d82@o1187002.ingest.sentry.io/6307588";
+                       o.Environment = IsDebugBuild ? "Debug" : "Release";
+                       o.Debug = IsDebugBuild;
+                       o.TracesSampleRate = 1.0;
+                       o.AttachStacktrace = true;
+                   });
+            SentrySdk.ConfigureScope(scope =>
+            {
+                scope.SetTag("os", Environment.OSVersion.ToString());
+                scope.SetTag("arch", Environment.Is64BitOperatingSystem ? "64-bit" : "32-bit");
+                scope.SetTag("version", AssemblyProductVersion);
+                scope.Platform = Environment.OSVersion.Platform.ToString();
+                scope.SetTag("build_type", IsDebugBuild ? "Debug" : "Release");
+                scope.SetExtra("CommandLine", Environment.CommandLine);
+            });
+            {
+                Logger.Info(SentrySdk.IsEnabled ? "Sentry is enabled" : "Sentry is disabled");
+            }
+        }
     }
 }
--- VRC-OSC-AudioEars/Program.cs
+++ VRC-OSC-AudioEars/Program.cs
@@ -1,52 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Threading.Tasks;
 using BuildSoft.VRChat.Osc;
-using BuildSoft.VRChat.Osc.Avatar;
 using NAudio.CoreAudioApi;
 using NAudio.Wave;
+using Octokit;
+using Sentry;
 
 namespace VRC_OSC_AudioEars
 {
     public static class VRChatOscAudio
     {
+        
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        
         private const string AudioVolumeParameter = "audio_volume";
         private const string AudioDirectionParameter = "audio_direction";
 
+        
         private static float _leftEarSmoothedVolume = 0;
         private static float _rightEarSmoothedVolume = 0;
         private static float _leftEarIncomingVolume = 0;
         private static float _rightEarIncomingVolume = 0;
         private static float _direction = 0;
         private static MMDeviceEnumerator enumerator = new();
-        private static MMDevice _device = null;
-        private static WasapiLoopbackCapture _capture = null;
+        private static MMDevice? _device = null;
+        private static WasapiLoopbackCapture? _capture = null;
         private static int _bytesPerSample = 0;
+        
+
         public static async Task Main(string[] args)
         {
-            Console.Title = "VRChat OSC Audio";
-            Console.WriteLine("Starting...");
+            Console.Title = "VRChat OSC Audio Reaction";
+            Logger.Info("Starting...");
+            Helpers.InitSentry();
+            Logger.Info($"Version: {Helpers.AssemblyProductVersion}");
             // Get Default audio device
-            _device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
-            _capture = new WasapiLoopbackCapture(_device);
-            _capture.DataAvailable += OnDataAvailable;
-            _capture.RecordingStopped += OnRecordingStopped;
-            _bytesPerSample = _capture.WaveFormat.BitsPerSample / 8;
-            _capture.StartRecording();
-
-            Console.WriteLine("Device: " + _device.FriendlyName + " Bitrate: " + _capture.WaveFormat.BitsPerSample + " SampleRate: " + _capture.WaveFormat.SampleRate);
-
-            Console.WriteLine("Waiting for VRChat... Please enable OSC in the VRChat action menu.");
-            Console.WriteLine("If OSC is Already enabled, turn it off and back on.");
-            // ReSharper disable once NotAccessedVariable
-            OscAvatarConfig config = await OscAvatarConfig.WaitAndCreateAtCurrentAsync();
-            Console.WriteLine("Started");
+            await SetUpAudio().ConfigureAwait(false);
+            await CheckGitHubNewerVersion().ConfigureAwait(false); // Don't wait for it
+            Logger.Info("Please enable OSC in the VRChat action menu.");
 
 
             while (true)
             {
                 try
                 {
-                    config = await OscAvatarConfig.WaitAndCreateAtCurrentAsync();
                     _leftEarSmoothedVolume = Helpers.VRCClampedLerp(_leftEarSmoothedVolume, _leftEarIncomingVolume, 0.3f);
                     _rightEarSmoothedVolume = Helpers.VRCClampedLerp(_rightEarSmoothedVolume, _rightEarIncomingVolume, 0.3f);
                     if (float.IsNaN(_leftEarSmoothedVolume) || float.IsNaN(_rightEarSmoothedVolume) || float.IsInfinity(_leftEarSmoothedVolume) || float.IsInfinity(_rightEarSmoothedVolume))
@@ -56,7 +56,6 @@ namespace VRC_OSC_AudioEars
                         _rightEarSmoothedVolume = 0;
                     }
                     _direction = Helpers.VRCClamp(-(_leftEarSmoothedVolume * 2) + (_rightEarSmoothedVolume * 2) + 0.5f);
-                    //Write all values to the console
                     OscParameter.SendAvatarParameter(AudioDirectionParameter, _direction);;
                     OscParameter.SendAvatarParameter(AudioVolumeParameter, (_leftEarSmoothedVolume + _rightEarSmoothedVolume) / 2);
                 }
@@ -64,8 +63,8 @@ namespace VRC_OSC_AudioEars
                 {
                     // Often errors when trying to send a value while changing avatars
                     Console.WriteLine("Error: " + e.Message);
+                    SentrySdk.CaptureException(e);
                     await Task.Delay(2000);
-
                 }
 
 
@@ -75,43 +74,43 @@ namespace VRC_OSC_AudioEars
             }
         }
 
-        private static float _left = 0;
-        private static float _right = 0;
+        private static float _leftRaw = 0;
+        private static float _rightRaw = 0;
         private static bool _shouldUpdate = false;
         static void OnDataAvailable(object sender, WaveInEventArgs args)
         {
             if (_shouldUpdate)
             {
                 _shouldUpdate = false;
-                _left = 0;
-                _right = 0;
+                _leftRaw = 0;
+                _rightRaw = 0;
                 // get the RMS of the left and right channels independently for 32 bit audio. ALso Githun Copilot go burr
                 if (_bytesPerSample == 4)
                 {
                     for (int i = 0; i < args.BytesRecorded; i += _bytesPerSample * 2)
                     {
-                        _left += Math.Abs(BitConverter.ToSingle(args.Buffer, i));
-                        _right += Math.Abs(BitConverter.ToSingle(args.Buffer, i + _bytesPerSample));
+                        _leftRaw += Math.Abs(BitConverter.ToSingle(args.Buffer, i));
+                        _rightRaw += Math.Abs(BitConverter.ToSingle(args.Buffer, i + _bytesPerSample));
                     }
                 }
                 else if (_bytesPerSample == 2)
                 {
                     for (int i = 0; i < args.BytesRecorded; i += _bytesPerSample * 2)
                     {
-                        _left += Math.Abs(BitConverter.ToInt16(args.Buffer, i));
-                        _right += Math.Abs(BitConverter.ToInt16(args.Buffer, i + _bytesPerSample));
+                        _leftRaw += Math.Abs(BitConverter.ToInt16(args.Buffer, i));
+                        _rightRaw += Math.Abs(BitConverter.ToInt16(args.Buffer, i + _bytesPerSample));
                     }
                 }
                 else
                 {
                     for (int i = 0; i < args.BytesRecorded; i += _bytesPerSample)
                     {
-                        _left += Math.Abs(BitConverter.ToInt32(args.Buffer, i));
-                        _right += Math.Abs(BitConverter.ToInt32(args.Buffer, i + _bytesPerSample));
+                        _leftRaw += Math.Abs(BitConverter.ToInt32(args.Buffer, i));
+                        _rightRaw += Math.Abs(BitConverter.ToInt32(args.Buffer, i + _bytesPerSample));
                     }
                 }
-                _leftEarIncomingVolume = _left / (args.BytesRecorded / _bytesPerSample);
-                _rightEarIncomingVolume = _right / (args.BytesRecorded / _bytesPerSample);
+                _leftEarIncomingVolume = _leftRaw / (args.BytesRecorded / _bytesPerSample);
+                _rightEarIncomingVolume = _rightRaw / (args.BytesRecorded / _bytesPerSample);
 
                 _leftEarIncomingVolume *= 10;
                 _rightEarIncomingVolume *= 10;
@@ -119,10 +118,46 @@ namespace VRC_OSC_AudioEars
         }
         static void OnRecordingStopped(object sender, StoppedEventArgs e)
         {
-            _capture.Dispose();
-            _device.Dispose();
+            if (_capture != null) _capture.Dispose();
+            if (_device != null) _device.Dispose();
             Environment.Exit(0);
         }
+        public static Task SetUpAudio()
+        {
+            _device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+            _capture = new WasapiLoopbackCapture(_device);
+            _capture.DataAvailable += OnDataAvailable!;
+            _capture.RecordingStopped += OnRecordingStopped!;
+            _bytesPerSample = _capture.WaveFormat.BitsPerSample / 8;
+            _capture.StartRecording();
+            Logger.Info("Device: " + _device.FriendlyName + " Bitrate: " + _capture.WaveFormat.BitsPerSample + " SampleRate: " + _capture.WaveFormat.SampleRate);
+            return Task.CompletedTask;
+        }
+        private static async Task CheckGitHubNewerVersion()
+        {
+            GitHubClient client = new GitHubClient(new ProductHeaderValue("SomeName"));
+            IReadOnlyList<Release> releases = await client.Repository.Release.GetAll("Codel1417", "VRC-OSC-Audio-Reaction");
+    
+            if (Helpers.AssemblyProductVersion != "" && releases.Count > 0)
+            {
+                Version latestGitHubVersion = new Version(releases[0].TagName);
+                Version localVersion = new Version(Helpers.AssemblyProductVersion); //Replace this with your local version. 
+                int versionComparison = localVersion.CompareTo(latestGitHubVersion);
+                if (versionComparison < 0)
+                {
+                    Logger.Warn("A new version of VRC-OSC-Audio-Reaction is available!");
+                }
+                else
+                {
+                    Logger.Info("You are running the latest version of VRC-OSC-Audio-Reaction!");
+                }
+            }
+            else
+            {
+                Logger.Error("Could not check for updates.");
+                SentrySdk.CaptureException(new VersionNotFoundException("Unable to check for updates."));
 
+            }
+        }
     }
 }
