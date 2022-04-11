c7e0f5a2a7a4763f6509ce7d2066218bb3fe3359
Codel1417 <austinrichards98@gmail.com>
2022-04-10 20:05:22 -0700

    add sentry WFP exception capture

--- VRC-OSC-AudioEars/App.xaml.cs
+++ VRC-OSC-AudioEars/App.xaml.cs
@@ -1,9 +1,26 @@
-﻿namespace VRC_OSC_AudioEars
+﻿using Sentry;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace VRC_OSC_AudioEars
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App
+    public partial class App : Application
     {
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            SentrySdk.CaptureException(e.Exception);
+
+            e.Handled = true;
+        }
     }
+
+
 }
