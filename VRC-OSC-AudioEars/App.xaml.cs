0338f48f066ac5bb6f3594a059c814396db16f2a
Codel1417 <awoo@codel1417.xyz>
2022-05-22 20:10:49 -0700

    Prevent multiple instances from opening

--- VRC-OSC-AudioEars/App.xaml.cs
+++ VRC-OSC-AudioEars/App.xaml.cs
@@ -1,6 +1,9 @@
-﻿using Sentry;
+﻿using System;
+using System.Diagnostics;
+using Sentry;
 using System.Windows;
 using System.Windows.Threading;
+using NLog;
 
 namespace VRC_OSC_AudioEars
 {
@@ -9,8 +12,15 @@ namespace VRC_OSC_AudioEars
     /// </summary>
     public partial class App : Application
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public App()
         {
+            if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1)
+            {
+                Logger.Error("App already running!");
+                Environment.Exit(1);
+            }
             DispatcherUnhandledException += App_DispatcherUnhandledException;
         }
 
